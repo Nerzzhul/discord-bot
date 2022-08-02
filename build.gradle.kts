a6a4f30b98d980c83c71750a5a040255b923a9b8
hidden <hiddenwebuser@gmail.com>
2022-08-02 12:51:07 +0300

    include JDA #30

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,12 @@ buildscript {
 
 plugins {
     val springBootVersion = "2.7.1"
+//    val jdaVersion = "5.0.0-alpha.17"
     id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
     id("org.springframework.boot") version springBootVersion apply false
     java
 }
+
 
 allprojects {
     group = "com.ebyte"
@@ -17,12 +19,14 @@ allprojects {
 
     repositories {
         mavenCentral()
+            maven("https://m2.dv8tion.net/releases")
     }
 }
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("net.dv8tion:JDA:5.0.0-alpha.17")
 }
 
 subprojects {
