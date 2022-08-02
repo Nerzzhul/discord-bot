d18184558351786a0ef26d0a15aef1b3fb5f8a40
hidden <hiddenwebuser@gmail.com>
2022-08-02 17:20:06 +0300

    Добавил либы, сконфигурировал бота

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,8 @@
 dependencies {
     implementation(project(":bot"))
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.flywaydb:flyway-core:9.0.4")
 }
 
 tasks.bootJar {
