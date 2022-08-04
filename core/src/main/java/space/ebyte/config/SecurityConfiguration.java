0da002f5d2b394a1ce903c07df7364a99c000fb8
s.bubnov <s.bubnov@technolab.com.ru>
2022-08-04 14:50:10 +0300

    config OAuth2AccessTokenResponseClient

--- core/src/main/java/space/ebyte/config/SecurityConfiguration.java
+++ core/src/main/java/space/ebyte/config/SecurityConfiguration.java
@@ -3,16 +3,23 @@ package space.ebyte.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
 import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.client.RestTemplate;
 import space.ebyte.oauth2.DiscordOAuth2User;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -34,10 +41,23 @@ public class SecurityConfiguration {
 
     @Bean
     public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService() {
-        RestTemplate restTemplate = builder.build();
-        restTemplate.setErrorHandler(new OAuth2ErrorResponseErrorHandler());
         var delegate = new DefaultOAuth2UserService();
-        delegate.setRestOperations(restTemplate);
+        delegate.setRestOperations(getRestTemplate());
         return request -> new DiscordOAuth2User(delegate.loadUser(request));
+    }
+
+    @Bean
+    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient() {
+        var restTemplate = getRestTemplate();
+        restTemplate.setMessageConverters(Arrays.asList(new FormHttpMessageConverter(), new OAuth2AccessTokenResponseHttpMessageConverter()));
+        var client = new DefaultAuthorizationCodeTokenResponseClient();
+        client.setRestOperations(restTemplate);
+        return client;
+    }
+
+    private RestTemplate getRestTemplate() {
+        var restTemplate = builder.build();
+        restTemplate.setErrorHandler(new OAuth2ErrorResponseErrorHandler());
+        return restTemplate;
     }
 }
