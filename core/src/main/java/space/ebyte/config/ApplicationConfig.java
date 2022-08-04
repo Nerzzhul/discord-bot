fc536591e033062b1a48fb9f2131253a649897e8
s.bubnov <s.bubnov@technolab.com.ru>
2022-08-04 10:41:47 +0300

    add User-Agent header

--- core/src/main/java/space/ebyte/config/ApplicationConfig.java
+++ core/src/main/java/space/ebyte/config/ApplicationConfig.java
@@ -1,21 +1,34 @@
 package space.ebyte.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestOperations;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class ApplicationConfig {
 
+    @Value("${discord.user-agent:Discord Bot (http://localhost:8080/)}")
+    private String userAgent;
+
     @Bean
-    public RestOperations restOperations(RestTemplateBuilder builder,
-                                         @Value("${discord.user-agent:Discord Bot (http://localhost:8080/)}") String userAgent) {
-        builder.additionalRequestCustomizers(request ->
-                request.getHeaders()
-                        .add(HttpHeaders.USER_AGENT, userAgent));
-        return builder.build();
+    public RestTemplateBuilder restTemplateBuilder() {
+        ClientHttpRequestInterceptor interceptor = (request, body, execution) -> {
+            request.getHeaders().add(HttpHeaders.USER_AGENT, userAgent);
+            return execution.execute(request, body);
+        };
+        return new RestTemplateBuilder(restTemplate -> restTemplate.setInterceptors(List.of(interceptor)));
+    }
+
+    @Bean
+    public RestOperations restOperations() {
+        return restTemplateBuilder().build();
     }
 }
--- core/src/main/java/space/ebyte/config/SecurityConfiguration.java
+++ core/src/main/java/space/ebyte/config/SecurityConfiguration.java
@@ -1,23 +1,24 @@
 package space.ebyte.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 import space.ebyte.oauth2.DiscordOAuth2User;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
-    private final RestOperations restOperations;
+    private final RestTemplateBuilder builder;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -33,8 +34,10 @@ public class SecurityConfiguration {
 
     @Bean
     public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService() {
+        RestTemplate restTemplate = builder.build();
+        restTemplate.setErrorHandler(new OAuth2ErrorResponseErrorHandler());
         var delegate = new DefaultOAuth2UserService();
-        delegate.setRestOperations(restOperations);
+        delegate.setRestOperations(restTemplate);
         return request -> new DiscordOAuth2User(delegate.loadUser(request));
     }
 }
