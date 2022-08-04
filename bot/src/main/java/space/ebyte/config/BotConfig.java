6e75cceb559bdab3a7713fdb4210360f17feacdd
s.bubnov <s.bubnov@technolab.com.ru>
2022-08-04 16:59:06 +0300

    fix package name

--- bot/src/main/java/space/ebyte/config/BotConfig.java
+++ bot/src/main/java/space/ebyte/config/BotConfig.java
@@ -1,9 +1,7 @@
-package space.ebyte.Config;
+package space.ebyte.config;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
-import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.ChunkingFilter;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
